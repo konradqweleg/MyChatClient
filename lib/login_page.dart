4665c29cfc6d2ec639519bb607de08d98e6a2660
Konrad Groń <konradnrog@gmail.com>
2023-07-17 21:23:57 +0200

    create new prototype login page

---
+++ lib/login_page.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:my_chat_client/resources/image_resources.dart';
+import 'package:my_chat_client/style/main_style.dart';
+import 'package:my_chat_client/resources/text_resources.dart';
+import 'PageRouteTransition.dart';
+import 'login.dart';
+
+void main() {
+  runApp(
+      const Directionality(
+          textDirection: TextDirection.ltr,
+          child: Scaffold(body:MainPanel())
+  ));
+}
+
+class StartPanel extends StatefulWidget {
+  const StartPanel({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return StartPanelState();
+  }
+}
+
+class StartPanelState extends State<StartPanel> {
+  @override
+  void initState() {
+    PageRouteTransition.transitionAfterDelay(
+        context: context, destination: const LoginPanel()
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MainPanel();
+  }
+}
+
+//Placeholder fajnie pokazuje wielksoci
+class MainPanel extends StatelessWidget {
+  const MainPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          const Expanded(
+            flex: 1,
+            child: SizedBox(),
+          ),
+          const BigAppLogo(),
+          const Spacer(),
+          Container(
+            margin: const EdgeInsets.all(20),
+            child: const NameApp(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BigAppLogo extends StatelessWidget {
+  static const imageDimension = 250.0;
+
+  const BigAppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(
+      image: AssetImage(ImageResources.logoApp),
+      width: imageDimension,
+      height: imageDimension,
+    );
+  }
+}
+
+class NameApp extends StatelessWidget {
+  static const letterSpacingInNameApp = 0.5;
+  static const nameAppFontSize = 30.0;
+  static const nameAppNameMargin = 20.0;
+
+  const NameApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      TextResources.nameApp,
+      style: GoogleFonts.inter(
+        textStyle: const TextStyle(
+            color: Color(MainAppStyle.mainColorApp),
+            letterSpacing: letterSpacingInNameApp,
+            fontSize: nameAppFontSize,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
