50144e8b8e86a6d7cfe7b59d58961473768fb4c5
Konrad Groń <konradnrog@gmail.com>
2024-09-22 22:01:49 +0200

    Added logger lib to project.

--- lib/http/http_helper.dart
+++ lib/http/http_helper.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 
 class HttpHelper {
-
-
+  var logger = Logger();
   final http.Client httpClient;
+
   HttpHelper(this.httpClient);
 
   Future<dynamic> executeHttpRequestWithTimeout(
@@ -16,6 +17,8 @@ class HttpHelper {
     Duration timeoutDuration = const Duration(seconds: 3),
   }) async {
     try {
+      logger.i(
+          'Request: $url  Body: $body  Headers: $headers  Timeout: $timeoutDuration');
 
       var response;
       try {
@@ -31,18 +34,22 @@ class HttpHelper {
         throw TimeoutException('Request timed out');
       }
 
-
-
       if (response is http.Response) {
+        logger.i('Response: ${response.body} StatusCode: ${response.statusCode}');
+
         if (response.statusCode == 200 || response.statusCode == 400 || response.statusCode == 401) {
           return response;
         } else {
           throw Exception('Request failed with status: ${response.statusCode}');
         }
+
       } else {
+        logger.e("Unknown response");
         throw Exception('Unknown response');
       }
+
     } catch (e) {
+      logger.e("Request error $e");
       throw Exception('Request failed: $e');
     }
   }
